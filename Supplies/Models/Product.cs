ebc3ae3af5a152ebf81bf0e8f83257eb6504e8b1
Mike Tucker <miket@operaglassnetworks.com>
2018-07-24 19:49:20 -0400

    fixed product view and model to auto generate ID

--- Supplies/Models/Product.cs
+++ Supplies/Models/Product.cs
@@ -8,7 +8,7 @@ namespace Supplies.Models
 {
     public class Product
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        //[DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ProductID { get; set; }
         public string Name { get; set; }
 
